f567bc8b2d4612475c56133d3577bcec21a46320
Brian Adams <2798669+brianadams2337@users.noreply.github.com>
2022-07-26 23:32:43 -0400

    Update Cart.js

--- components/Cart.js
+++ components/Cart.js
@@ -30,6 +30,7 @@ export default function Cart() {
         }),
         headers: { "Content-Type": "application/json" },
       });
+      console.log('checkout resp', checkresponse);
       console.log('checkout resp', checkresponse.data);
 
       const json = await response.json();
